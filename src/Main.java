401cd278783ce20e3020094754824f3b77360583
seancleve <seanycleve@gmail.com>
2024-09-20 10:11:50 -0400

    Lab 04 04

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,14 @@
 public class Main
 {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
+        int numOddEven = 2;
+
+        if (numOddEven % 2 == 0) {
+            System.out.println(numOddEven + " is Even.");
+        } else {
+            System.out.println(numOddEven + " is Odd.");
 
 
-
-        
+        }
     }
 }
